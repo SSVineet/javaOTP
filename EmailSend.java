f9f17d0c1d710a75368a689096a0dbfc17edd409
Maurya Vineet <44580462+vksmpic@users.noreply.github.com>
2018-12-17 22:39:01 +0530

    Add files via upload
    this generate automatic four digit OTP and send your Email
    and also you offline

---
+++ EmailSend.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * https://www.google.com/settings/security/lesssecureapps
+ */
+
+package javamailer;
+
+/**
+ *
+ * @author Naveen
+ */
+import java.util.*;
+
+public class EmailSend {
+
+    public static void main(String args[]){
+        opt obj1 = new opt();
+        SendOTP obj = new SendOTP();
+        obj.setVisible(true);
+
+       
+       
+    }
+}
---
+++ opt.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javamailer;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.internet.MimeMessage;
+import java.util.Random;
+
+/**
+ *
+ * @author MAURYA
+ */
+public class opt {
+
+    /**
+     *
+     * @param email
+     * 
+     */
+      String id;
+    public String opt1(String email){
+        try{
+            Random rand = new Random();
+            int random = (int )(Math.random() * 10000 + 1);
+            Scanner in = new Scanner(System.in);
+           // System.out.println("Email addresss");
+           // String email= in.nextLine();
+            
+           
+           id = String.format("%04d", rand.nextInt(10000));
+            
+            //System.out.printf("%04d%n", rand.nextInt(10000));
+            String host ="smtp.gmail.com" ;
+            String user = "kniterp178212@gmail.com";
+            String pass = "8171842600";
+            String to = email;
+            String from = "kniterp178212@gmail.com";
+            String subject = "KNIT ERP LOGIN VERIFICATION";
+            String messageText = "YOU ONE TIME OTP is "+id;
+            boolean sessionDebug = false;
+
+            Properties props = System.getProperties();
+
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.required", "true");
+
+          //  java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+            Session mailSession = Session.getDefaultInstance(props, null);
+            mailSession.setDebug(sessionDebug);
+            Message msg = new MimeMessage(mailSession);
+            msg.setFrom(new InternetAddress(from));
+            InternetAddress[] address = {new InternetAddress(to)};
+            msg.setRecipients(Message.RecipientType.TO, address);
+            msg.setSubject(subject); 
+            msg.setSentDate(new Date());
+            msg.setText(messageText);
+
+           Transport transport=mailSession.getTransport("smtp");
+           transport.connect(host, user, pass);
+           transport.sendMessage(msg, msg.getAllRecipients());
+           transport.close();
+           System.out.println("message send successfully"+" : "+id+"    (don't share)");
+           
+        }catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
+        return(id);
+        
+    }
+
+   
+}
